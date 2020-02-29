7b897df4ea025237d899e70e27fc6e9d63f98b13
Mohamadreza <m.reza.golbaba@gmail.com>
2020-02-29 12:12:04 +0330

    create tab navigation

---
+++ navigation/TopTabNavigator.js
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {TabBarIconCafee,TabBarIconRestaurant,TabBarIconSale,TabBarIconSearch} from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import createCSSStyleSheet from "react-native-web/dist/exports/StyleSheet/createCSSStyleSheet";
+import {StyleSheet} from "react-native";
+
+const Tab = createMaterialTopTabNavigator();
+const INITIAL_ROUTE_NAME = 'Home';
+
+
+function TopTabNavigator ({ navigation }) {
+
+    navigation.setOptions({headerStyle:styles.headerStyle});
+    return (
+        <Tab.Navigator
+            initialRouteName={INITIAL_ROUTE_NAME}
+            tabBarOptions={{
+                indicatorStyle: styles.indicator,
+                activeTintColor: 'white',
+                inactiveTintColor: 'grey',
+                activeBackgroundColor:'red',
+                style:styles.tabStyle,
+                showIcon: true,
+                showLabel: false,
+            }}
+        >
+
+            <Tab.Screen
+                name="Cofee"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIconCafee focused={focused} name="coffee" />,
+                }}
+            />
+            <Tab.Screen
+                name="Resources"
+                component={LinksScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIconRestaurant focused={focused} name="restaurant" />,
+                }}
+            />
+            <Tab.Screen
+                name="Disqaunt"
+                component={LinksScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIconSale focused={focused} name="sale" />,
+                }}
+            />
+            <Tab.Screen
+                name="search"
+                component={LinksScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIconSearch focused={focused} name="search1" />,
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+ function Coffee() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name="Cofee"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIconCafee focused={focused} name="coffee" />,
+                }}
+            />
+            <Tab.Screen
+                name="Resources"
+                component={LinksScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIconRestaurant focused={focused} name="restaurant" />,
+                }}
+            />
+            <Tab.Screen
+                name="Disqaunt"
+                component={LinksScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIconSale focused={focused} name="sale" />,
+                }}
+            />
+            <Tab.Screen
+                name="search"
+                component={LinksScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIconSearch focused={focused} name="search1" />,
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+export {
+    Coffee,
+    TopTabNavigator
+
+}
+
+
+const styles=StyleSheet.create({
+    headerStyle:{
+        backgroundColor:'#cb1b00',
+        borderBottomColor:'#cb1b00'
+    },
+    indicator:{
+        backgroundColor: '#000',
+        height: 50,
+        borderTopLeftRadius:15,
+        borderTopRightRadius:15,
+    },
+    tabStyle:{
+        backgroundColor: '#cb1b00',
+        borderTopColor:'#cb1b00'
+    }
+})
+
