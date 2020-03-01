57cf5e0ec0da0b92b2175db8f13c009a304c257e
Mohamadreza <m.reza.golbaba@gmail.com>
2020-03-01 09:22:52 +0330

    add style link screen

--- screens/LinkScreen/LinksScreen.js
+++ screens/LinkScreen/LinksScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import {styles} from './Style'
 
 export default function LinksScreen() {
   return (
@@ -44,31 +45,3 @@ function OptionButton({ icon, label, onPress, isLastOption }) {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fafafa',
-  },
-  contentContainer: {
-    paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
-  },
-});
