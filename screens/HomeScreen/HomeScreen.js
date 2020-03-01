0faff5d32ab3687eec8bdfb875b8810830e32efa
Mohamadreza <m.reza.golbaba@gmail.com>
2020-03-01 09:23:06 +0330

    add style home screen

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -3,14 +3,14 @@ import {Platform, StyleSheet, Text, TouchableOpacity, View, FlatList, Button} fr
 import { ScrollView } from 'react-native-gesture-handler';
 import * as WebBrowser from 'expo-web-browser';
 import {Container, Tabs, Tab, TabHeading, Icon,ScrollableTab,Row,Content,Grid,ListItem} from 'native-base';
-import logo from '../assets/images/robot-dev.png'
-import ListItems from '../components/common/ListItems'
-import WebModalComponent from '../components/web/WebModalComponent'
+import logo from '../../assets/images/robot-dev.png'
+import ListItems from '../../components/common/ListItems'
+import WebModalComponent from '../../components/web/WebModalComponent'
 import {connect } from 'react-redux'
-import {bindActionCreators} from "redux";
-import {getArticles} from "../utils/Redux/actions";
-import ModalComponent from "../components/Native/ModalComponent";
-import Modal from "react-native";
+import {getProducts} from "../../utils/Redux/actions";
+import ModalComponent from "../../components/Native/ModalComponent";
+import ModalItems from "../../components/web/ModalItems";
+import {styles} from './Style'
 
 class Home extends React.Component {
   constructor(props) {
@@ -23,12 +23,11 @@ class Home extends React.Component {
     }
   }
   componentDidMount() {
-    this.props.articlesActions();
+    this.props.productsActions();
   }
 
   openModal = (event,item) =>{
     event.persist();
-    console.log('iteeeeeee',item)
       this.setState({
         modalData:item
       },()=> this.setState({
@@ -43,7 +42,6 @@ class Home extends React.Component {
   }
   render() {
     const{currentTab,modalVisible}=this.state
-    console.log(currentTab,modalVisible,this.props.articles)
     return (
         <Container style={styles.container}>
           <Tabs initialPage={0} onChangeTab={(i) => this.setState({currentTab:i})} tabBarUnderlineStyle={{display: 'none'}}
@@ -54,7 +52,7 @@ class Home extends React.Component {
               <Text style={ currentTab.i===0?styles.ActiveText:styles.text}>All</Text></TabHeading>}>
               <View style={styles.wrapperItems}>
                 <FlatList
-                    data={this.props.articles}
+                    data={this.props.Products}
                     renderItem={({ item }) => (
                         <TouchableOpacity onPress={(event) => this.openModal(event,item)}>
                         <ListItems text={item.name} Img={{uri:item.image_url}} />
@@ -71,7 +69,7 @@ class Home extends React.Component {
                 style={currentTab.i===1?styles.ActiveText:styles.text}>Pizza</Text></TabHeading>}>
               <View style={styles.wrapperItems}>
                 <FlatList
-                    data={this.props.articles}
+                    data={this.props.Products}
                     renderItem={({ item }) => (
                         <TouchableOpacity onPress={(event) => this.openModal(event,item)}>
                           <ListItems text={item.name} Img={{uri:item.image_url}} />
@@ -87,7 +85,7 @@ class Home extends React.Component {
                 style={currentTab.i===2?styles.ActiveText:styles.text}>STEAK</Text></TabHeading>}>
               <View style={styles.wrapperItems}>
                 <FlatList
-                    data={this.props.articles}
+                    data={this.props.Products}
                     renderItem={({ item }) => (
                         <TouchableOpacity onPress={(event) => this.openModal(event,item)}>
                           <ListItems text={item.name} Img={{uri:item.image_url}} />
@@ -100,10 +98,16 @@ class Home extends React.Component {
             </Tab>
           </Tabs>
           {Platform.OS==='web'?
-              <WebModalComponent isVisible={this.state.isModalVisible} onBackdropPress={()=>this.closeModal()}/>
+              <ModalItems data={this.state.modalData&&this.state.modalData} isVisible={this.state.isModalVisible} onBackdropPress={()=>this.closeModal()}/>
           :
               <ModalComponent data={this.state.modalData&&this.state.modalData} visible={this.state.isModalVisible} onRequestClose={()=>this.closeModal()} />
           }
+          {/*{*/}
+          {/*  Platform.OS==='web'?*/}
+          {/*    <WebModalComponent data={this.state.modalData&&this.state.modalData} isVisible={this.state.isModalVisible} onBackdropPress={()=>this.closeModal()}/>*/}
+          {/*:*/}
+          {/*    <ModalComponent data={this.state.modalData&&this.state.modalData} visible={this.state.isModalVisible} onRequestClose={()=>this.closeModal()} />*/}
+          {/*}*/}
         </Container>
     );
   }
@@ -115,14 +119,14 @@ class Home extends React.Component {
 const mapStateToProps= (state)=> {
 
   return {
-    articles:state.articleReducer
+    Products:state.ProductsReducer
   }
 };
 // We can dispatch actions to the reducer and sagas
 const mapDispatchToProps= (dispatch)=> {
   return {
-    articlesActions:()=>{
-      dispatch(getArticles())
+    productsActions:()=>{
+      dispatch(getProducts())
     }
   };
 };
@@ -132,123 +136,3 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-
-
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-  activeSubTabStyle:{
-    backgroundColor:'#000',
-    color:'#FFF'
-  },
-  deActiveSubTabStyle:{
-    backgroundColor:'#000',
-    color:'#888888'
-  },
-  text:{
-    color:'#888888',
-    fontSize:16,
-    fontWeight:'bold'
-  },
-  ActiveText:{
-    color:'#FFF',
-    fontSize:16,
-    fontWeight:'bold'
-  },
-  wrapperItems:{
-    paddingLeft:10,
-    paddingRight:10
-  },
-  list: {
-    flexDirection: 'row',
-    flexWrap: 'wrap'
-  },
-
-});
---
+++ screens/HomeScreen/Style.js
@@ -0,0 +1,117 @@
+import {Platform, StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    developmentModeText: {
+        marginBottom: 20,
+        color: 'rgba(0,0,0,0.4)',
+        fontSize: 14,
+        lineHeight: 19,
+        textAlign: 'center',
+    },
+    contentContainer: {
+        paddingTop: 30,
+    },
+    welcomeContainer: {
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 20,
+    },
+    welcomeImage: {
+        width: 100,
+        height: 80,
+        resizeMode: 'contain',
+        marginTop: 3,
+        marginLeft: -10,
+    },
+    getStartedContainer: {
+        alignItems: 'center',
+        marginHorizontal: 50,
+    },
+    homeScreenFilename: {
+        marginVertical: 7,
+    },
+    codeHighlightText: {
+        color: 'rgba(96,100,109, 0.8)',
+    },
+    codeHighlightContainer: {
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        borderRadius: 3,
+        paddingHorizontal: 4,
+    },
+    getStartedText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        lineHeight: 24,
+        textAlign: 'center',
+    },
+    tabBarInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: { width: 0, height: -3 },
+                shadowOpacity: 0.1,
+                shadowRadius: 3,
+            },
+            android: {
+                elevation: 20,
+            },
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+        paddingVertical: 20,
+    },
+    tabBarInfoText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        textAlign: 'center',
+    },
+    navigationFilename: {
+        marginTop: 5,
+    },
+    helpContainer: {
+        marginTop: 15,
+        alignItems: 'center',
+    },
+    helpLink: {
+        paddingVertical: 15,
+    },
+    helpLinkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+    },
+    activeSubTabStyle:{
+        backgroundColor:'#000',
+        color:'#FFF'
+    },
+    deActiveSubTabStyle:{
+        backgroundColor:'#000',
+        color:'#888888'
+    },
+    text:{
+        color:'#888888',
+        fontSize:16,
+        fontWeight:'bold'
+    },
+    ActiveText:{
+        color:'#FFF',
+        fontSize:16,
+        fontWeight:'bold'
+    },
+    wrapperItems:{
+        paddingLeft:10,
+        paddingRight:10
+    },
+    list: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+
+});
